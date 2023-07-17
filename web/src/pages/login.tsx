a513bfe2d22e71618fdf40d4ba97cf3510eb25c9
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-07-17 19:40:38 -0300

    Login page

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,64 +1,87 @@
+import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
-import { EyeIcon } from "lucide-react";
+import { AuthContext } from "@/contexts/AuthContext";
 import Image from "next/image";
+import { FormEvent, useContext, useState } from "react";
+
+import rebanho from "../../public/rebanho.png";
 
 export default function Login() {
+  const [ email, setEmail ] = useState<string>("");
+  const [ password, setPassword ] = useState<string>("");
+
+  const { signIn } = useContext(AuthContext);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if(!email || !password) return
+
+    await signIn({ email, password });
+  }
+
   return (
     <div className="w-screen h-screen">
       <Image
         className="h-1/2"
-        src="/rebanho.png"
+        src={rebanho}
         alt="Rebanho bovino"
         width={512}
         height={512}
+        priority
       />
 
-      <div
-        className="h-1/2 flex flex-col items-center"
-      >
+      <form id="login" onSubmit={(e) => handleSubmit(e)}>
         <div
-          className="w-full mb-6 grid grid-rows-2 justify-center gap-3 mt-12"
+          className="mt-12 grid gap-6 justify-center"
         >
           <Input
             label="Email"
             type="text"
+            value={email}
+            autoComplete="true"
+            required
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <Input
             label="Senha"
             type="password"
-          >
-            <EyeIcon
-              className="mr-4 absolute right-0 bottom-0 -translate-y-1/3"
-              color="#888"
-              strokeWidth={1.5}
-            />
-          </Input>
-        </div>
-
-        <span
-          className="
-          w-32 text-center text-gray-300
-          before:block before:content[''] before:w-60 before:h-px before:-translate-x-14 before:translate-y-3 before:border-x-16 before:border-gray-300
-          "
-        >
-          Ou entre com
-        </span>
-
-        <button
-          className="w-80 h-10 bg-gray-100 rounded-lg m-6 flex items-center justify-center"
-        >
-          <Image
-            className="ml-4 absolute left-12"
-            src="/google.png"
-            alt="Google"
-            width={20}
-            height={20}
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+    
+          <Button
+            text="Entre"
+            type="submit"
+            form="login"
           />
 
-          Entrar com o Google
-        </button>
-      </div>
+          <span
+            className="
+            w-32 text-center text-gray-300 m-auto
+            before:block before:content[''] before:w-60 before:h-px before:-translate-x-14 before:translate-y-3 before:border-x-16 before:border-gray-300
+            "
+          >
+            Ou entre com
+          </span>
+
+          <button
+            className="w-full max-w-xs h-10 bg-gray-100 rounded-lg flex items-center justify-center relative"
+          >
+            <Image
+              className="ml-6 absolute left-0"
+              src="/google.png"
+              alt="Google"
+              width={20}
+              height={20}
+            />
+
+            Entrar com o Google
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
