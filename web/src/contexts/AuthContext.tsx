b57151aaedce76ddd070193d13c7c7263179c38b
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-07-20 13:00:54 -0300

    Create register function

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -7,6 +7,7 @@ type AuthContextType = {
   isAuthenticated: boolean;
   user: User | null;
   signIn: (data: SignInData) => Promise<void>;
+  register: (data: RegisterData) => Promise<void>;
 };
 
 type User = {
@@ -18,7 +19,13 @@ type User = {
 type SignInData = {
   email: string;
   password: string;
-};
+}
+
+type RegisterData = {
+  name: string;
+  email: string;
+  password: string;
+}
 
 type AuthContextProviderProps = {
   children: ReactNode;
@@ -34,12 +41,10 @@ export function AuthProvider({ children }: AuthContextProviderProps) {
   const isAuthenticated = !!user;
 
   useEffect(() => {
-    // Verificar se possui algum token nos cookies
     const { token } = parseCookies();
 
     if(!token) return;
 
-    // Se existir, buscar informações do usuário e salvar no estado
     api.get(`/users/${user?.id}`).then((response) => {
       setUser(response.data);
     });
@@ -47,19 +52,15 @@ export function AuthProvider({ children }: AuthContextProviderProps) {
 
  async function signIn({ email, password }: SignInData) {
    try {
-    // Autenticar usuário no backend
     const { data } = await api.post("/users/login", {
         email,
         password,
       });
     
-    // Salvar token do usuário nos cookies
     setCookie(undefined, "token", data.token);
 
-    // Adicionar token ao header das requisições
     api.defaults.headers['Authorization'] = `Bearer ${data.token}`;
   
-    // Salvar informações do usuário em um estado
     setUser(data);
 
     router.push("/");
@@ -68,8 +69,24 @@ export function AuthProvider({ children }: AuthContextProviderProps) {
   }
  }
 
+ async function register({ name, email, password }: RegisterData) {
+  try {
+    const { data } = await api.post("/users/register", {
+        name,
+        email,
+        password,
+      });
+    
+    setUser(data);
+
+    router.push("/");
+  } catch(err) {
+    console.log("register error");
+  }
+ }
+
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, register }}>
       {children}
     </AuthContext.Provider>
   );
